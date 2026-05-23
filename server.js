3d1b64165c49894facbdc92cf2c08eeb8fb7f553
Mahmud Haji <mahmudhaji2010@gmail.com>
2026-05-23 13:12:14 +0000

    503 Service Unavailable The server is temporarily busy, try again later!

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
 const port = process.env.PORT || 3000
-const app = next({ dev, hostname, port })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -21,6 +20,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
+    console.log(`> Ready on port ${port}`)
   })
 })
